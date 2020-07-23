318e072aa083ae23d087ae52e172ede5394f78cf
unknown <jg.aguilar10@gmail.com>
2020-07-23 17:39:35 -0500

    'update'

--- m121/docs/M121-3.js
+++ m121/docs/M121-3.js
@@ -35,3 +35,73 @@ db.movies.aggregate([
     }
   }
 ])
+
+//Lab - $unwind
+
+var pipeline = [
+  {
+    $match: {
+      languages: "English",
+      "imdb.rating": {$gte: 0}
+    }
+  },
+  {
+
+    $unwind: "$cast"
+
+  },
+  {
+    $group: {
+      _id: "$cast",
+      numFilms: {$sum: 1},
+      average: {$avg: "$imdb.rating"}
+    }
+  },
+  {
+    $sort:{
+  
+          numFilms: -1
+  
+        }  
+    },
+    {
+    $limit:1  
+    }    
+]
+
+//answer
+
+db.movies.aggregate([
+  {
+    $match: {
+      languages: "English"
+    }
+  },
+  {
+    $project: { _id: 0, cast: 1, "imdb.rating": 1 }
+  },
+  {
+    $unwind: "$cast"
+  },
+  {
+    $group: {
+      _id: "$cast",
+      numFilms: { $sum: 1 },
+      average: { $avg: "$imdb.rating" }
+    }
+  },
+  {
+    $project: {
+      numFilms: 1,
+      average: {
+        $divide: [{ $trunc: { $multiply: ["$average", 10] } }, 10]
+      }
+    }
+  },
+  {
+    $sort: { numFilms: -1 }
+  },
+  {
+    $limit: 1
+  }
+])
